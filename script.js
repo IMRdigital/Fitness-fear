29143b3b7f54854c78929439f57e4a8a1958a746
IMRdigital <shaikimran818483@gmail.com>
2026-06-24 20:32:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Fitness Fear JavaScript
+
+console.log("Fitness Fear Loaded Successfully!");
+
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const top = card.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
